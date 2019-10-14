d25ae27c74ab21ab47d70ab21ef6d81e90f34860
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2019-10-14 15:19:56 +0300

    [~]

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/juev/jekyll2hugo
+
+go 1.13
+
+require gopkg.in/yaml.v2 v2.2.4
--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"regexp"
+
+	"gopkg.in/yaml.v2"
 )
 
+const fmMagic = "---\n"
+
+var frontMatterMatcher = regexp.MustCompile(`(?s)^---\n(.+?\n)---\n+`)
+var emptyFontMatterMatcher = regexp.MustCompile(`(?s)^---\n+---\n+`)
+
+// T struct for unmarshal
+type T struct {
+	Layout string
+	Title  string
+	Date   string
+	Image  string
+	Tags   []string
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	fmt.Println("Hellow World!")
+	source, err := ioutil.ReadFile("./example/2019-09-08-programming-language-performance.markdown")
+	check(err)
+	fm := T{}
+
+	source = bytes.Replace(source, []byte("\r\n"), []byte("\n"), -1)
+	if match := frontMatterMatcher.FindSubmatchIndex(source); match != nil {
+		fmt.Println(match)
+		fmt.Println(string(source[match[2]:match[3]]))
+		if err = yaml.Unmarshal(source[match[2]:match[3]], &fm); err != nil {
+			return
+		}
+	}
+	// err = yaml.Unmarshal([]byte(dat), &t)
+	// check(err)
+	// fmt.Print(string(dat))
+	fmt.Printf("--- t:\n%v\n\n", fm)
+	d, err := yaml.Marshal(&fm)
+	check(err)
+	fmt.Printf("--- m dump:\n%s\n\n", string(d))
 }
